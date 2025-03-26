5c078ea2fc0746f5ed7d7c13b0002d7c7e9d9775
Ljupche Vasilev <ljupche.vasilev@gmail.com>
2025-03-26 18:31:10 +0000

    netlfiy functions

--- src/swagger/swagger.ts
+++ src/swagger/swagger.ts
@@ -3,7 +3,9 @@ import yaml from "js-yaml"
 import fs from "fs"
 import path from "path"
 
-const swaggerFilePath = path.join(process.env.LAMBDA_TASK_ROOT!, "api", "api-doc.yaml")
+const swaggerFilePath = process.env.LAMBDA_TASK_ROOT
+  ? path.resolve(process.env.LAMBDA_TASK_ROOT, "./netlify/functions/api/api-doc.yaml")
+  : path.join(__dirname, "../docs/api-doc.yaml")
 const swaggerSpec = yaml.load(
   fs.readFileSync(swaggerFilePath, "utf8"),
 ) as Record<string, any>
