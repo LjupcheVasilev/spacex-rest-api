532df2c121a04ca8e150ffac52cd88f4d0aa7938
Ljupche Vasilev <ljupche.vasilev@gmail.com>
2025-03-26 18:00:22 +0000

    use esbuild.mjs + copy api-docs.yaml

--- esbuild.mjs
+++ esbuild.mjs
@@ -1,8 +1,12 @@
 import * as esbuild from 'esbuild'
+import fs from 'fs/promises'
 
 await esbuild.build({
     entryPoints: ['./netlify/functions/api.ts'],
     bundle: true,
     platform: "node",
-    outfile: 'dist/out.js',
+    outfile: 'dist/index.js',
 })
+
+// Copy API docs
+await fs.cp('./src/docs/api-doc.yaml', 'dist/api-docs.yaml')
