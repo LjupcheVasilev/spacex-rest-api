2cfd2d9a78e387a21d60bb830014f1cf94487d9a
Ljupche Vasilev <ljupche.vasilev@gmail.com>
2025-04-02 15:55:35 +0100

    remove yaml loader

--- src/swagger/swagger.ts
+++ src/swagger/swagger.ts
@@ -1,11 +1,16 @@
 import swaggerUi from "swagger-ui-express"
 import yaml from "js-yaml"
+import fs from "fs"
+import path from "path"
 
-import swaggerSpec from "../../docs/api-doc.yaml?raw"
+const swaggerFilePath = process.env.LAMBDA_TASK_ROOT
+  ? path.resolve(process.env.LAMBDA_TASK_ROOT, "./api-doc.yaml")
+  : path.join(__dirname, "/docs/api-doc.yaml")
+const swaggerSpec = yaml.load(
+  fs.readFileSync(swaggerFilePath, "utf8"),
+) as Record<string, any>
 
-const parsedSpec = yaml.load(swaggerSpec) as Record<string, any>
-
-export const swaggerDocs = swaggerUi.setup(parsedSpec, {
+export const swaggerDocs = swaggerUi.setup(swaggerSpec, {
   swaggerOptions: {
     supportedSubmitMethods: ["get", "post", "put", "delete"],
   },
